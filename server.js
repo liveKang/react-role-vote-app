308b74168e047c6167970796439c1f3b089492e1
rain <1368189893@qq.com>
2016-10-28 18:06:29 +0800

    app

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');   //http请求日志
+var bodyParser = require('body-parser');   //渲染POST请求数据
+
+var swig = require('swig');
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./app/routes');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+  Router.run(routes, req.path, function(Handler) {
+    var html = React.renderToString(React.createElement(Handler));
+    var page = swig.renderFile('views/index.html', { html: html });
+    res.send(page);
+  });
+});
+
+app.listen(app.get('port'),function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
