1c80a1aac75f5c405172b8b079c8a02469d2324f
rain <1368189893@qq.com>
2016-10-31 16:09:21 +0800

    finish socket onlineUsers number

--- server.js
+++ server.js
@@ -24,6 +24,25 @@ app.use(function(req, res) {
   });
 });
 
-app.listen(app.get('port'),function(){
+/**
+ * Socket.io stuff
+ */
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function(socket){
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function() {
+    onlineUsers--;
+    io.sockets.emit('onlineUsers',{ onlineUsers: onlineUsers });
+  });
+});
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
