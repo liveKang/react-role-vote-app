faa6b32044f0472ea8cc30b1f1c276854dcec285
杨能康 <yangnengkang@yangnenangdeMBP.lan>
2016-11-03 17:23:02 +0800

    add express routes

--- server.js
+++ server.js
@@ -139,6 +139,161 @@ app.post('api/character', function(req, res, next) {
      });
  });
 
+ /**
+  * PUT /api/characters
+  * update winning and losing count for both characters.
+  */
+app.put('api/characters', function(req, res, next) {
+  var winner = req.body.winner;
+  var loser = req.body.loser;
+
+  if(!winner || !loser) {
+    return res.status(400).send({ message: 'Voting requires two characters.' })
+  }
+
+  if(winner === loser) {
+    return res.status(400).send({ message: 'Cannot vote for and against the same character.' });
+  }
+
+  async.parallel([
+    function(callback){
+      Character.findOne({ characterId: winner }, function(err, winner) {
+        callback(err, winner);
+      });
+    }
+  ],
+  function(err, results) {
+    if (err) return next(err);
+
+    var winner = results[0];
+    var loser = results[1];
+
+    if(!winner || !loser) {
+      return res.status(404).send({ message: 'One of the characters no longer exists.' });
+    }
+
+    if(winner.voted || loser.voted) {
+      return res.status(200).end();
+    }
+
+    async.parallel([
+      function(callback) {
+        winner.wins ++;
+        winner.voted = true;
+        winner.random = [Math.random(), 0];
+        winner.save(function(err) {
+          callback(err);
+        });
+      },
+      function(callback) {
+        loser.losses++;
+        loser.voted = true;
+        loser.random = [Math.random(), 0];
+        loser.save(function (err) {
+          callback(err);
+        });
+      }
+    ], function(err) {
+      if(err) return next (err);
+      res.status(200).end();
+    });
+  });
+});
+
+/**
+ * GET /api/characters/count
+ * Return the total number of characters.
+ */
+app.get('api/characters/count', function(req, res, next) {
+  Character.count({ }, function(err, count) {
+    if (err) return next(err);
+    res.send({ count: count });
+  });
+});
+
+/**
+ * GET /api/characters/search
+ * Looks up a character by name. (case-insensitive)
+ */
+app.get('api/characters/search', function(req, res, next) {
+  var characterName = new RegExp(req.query.name, 'i');
+
+  Character.findOne({ name: characterName }, function(err, character) {
+    if (err) return next (err);
+
+    if (!character) {
+      return res.status(404).send({ message: 'Character not found.' });
+    }
+
+    res.send(charachter);
+  });
+});
+
+/**
+ * GET /api/characters/:id
+ * Returns detailed charachter information.
+ */
+app.get('/api/characters/:id', function(req, res, next){
+  var id = req.params.id;
+
+  Character.findOne({ characterId: id }, function(err, character) {
+    if (err) return next(err);
+
+    if(!character) {
+      return res.status(404).send({ message: 'Character not found.' });
+    }
+
+    res.send(character);
+  });
+});
+
+/**
+ * GET /api/characters/top
+ * Return 100 highest ranked characters. Filter by gender, race and bloodline.
+ */
+app.get('/api/characters/top', function(req, res, next) {
+  var params = req.query;
+  var conditions = {};
+
+  _.each(params, function(value, key) {
+    conditions[key] = new RegExp('^' + value + '$', 'i');
+  });
+
+  Character
+    .find(conditions)
+    .sort('-wins')  //sort in descending order (highest wins on top)
+    .limit(100)
+    .exec(function(err, characters) {
+      if (err) return next(err);
+
+      //sort by winneing percentage
+      characters.sort(function(a, b){
+        if(a.wins / (a.wins + a.losses) < b.wins / (b.wins + b.losses)) { return 1; }
+        if(a.wins / (a.wins + a.losses) > b.wins / (b.wins + b.losses)) { return -1; }
+        return 0;
+      });
+
+      res.send(characters);
+    });
+});
+
+/**
+ * GET /api/character/shame
+ * Return 100 lowest ranked characters.
+ */
+app.get('/api/characters/shame', function(req, res, next) {
+  Character
+    .find()
+    .sort('-losses')
+    .limit(100)
+    .exec(function(err, characters) {
+      if (err) return next(err);
+      res.send(characters);
+    });
+});
+
+//POST /api/report
+
 app.use(function(req, res) {
   Router.run(routes, req.path, function(Handler) {
     var html = React.renderToString(React.createElement(Handler));
